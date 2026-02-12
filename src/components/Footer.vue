d81b84da614f2c2bc63f27f3dc3a69e3fd9b455f
NullSquad-RichardFox <richardfox.aurora@gmail.com>
2026-02-12 22:41:12 +0100

    Updating global styles

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,23 +4,19 @@ import { ref } from "vue"
 const emit = defineEmits(['cardio', 'gym', 'diet']);
 
 const activeId = ref(1)
-const haloPos = ref(30)
 
 const cardioClick = () => {
     activeId.value = 0;
-    haloPos.value = -3.5;
     emit('cardio');
 };
 
 const gymClick = () => {
     activeId.value = 1;
-    haloPos.value = 30;
     emit('gym');
 };
 
 const dietClick = () => {
     activeId.value = 2;
-    haloPos.value = 63.5;
     emit('diet');
 };
 
@@ -28,15 +24,16 @@ const dietClick = () => {
 
 <template>
     <div class="footer">
-        <div class="halo"></div>
-        <div class="footer-element" :class="[activeId == 0 ? 'active' : 'in-active']" @click="cardioClick">
-            <img src="../assets/cardio.png" alt="">
-        </div>
-        <div class="footer-element" :class="[activeId == 1 ? 'active' : 'in-active']" @click="gymClick">
-            <img src="../assets/gym.png" alt="">
-        </div>
-        <div class="footer-element" :class="[activeId == 2 ? 'active' : 'in-active']" @click="dietClick">
-            <img src="../assets/diet.png" alt="">
+        <div class="holder">
+            <div class="footer-element" :class="[activeId == 0 ? 'active' : 'in-active']" @click="cardioClick">
+                <img src="../assets/cardio.png" alt="">
+            </div>
+            <div class="footer-element" :class="[activeId == 1 ? 'active' : 'in-active']" @click="gymClick">
+                <img src="../assets/gym.png" alt="">
+            </div>
+            <div class="footer-element" :class="[activeId == 2 ? 'active' : 'in-active']" @click="dietClick">
+                <img src="../assets/diet.png" alt="">
+            </div>
         </div>
     </div>
 </template>
@@ -48,34 +45,18 @@ const dietClick = () => {
     width: 100vw;
     left: 0;
     bottom: 0;
-    display: grid;
-    grid-template-columns: auto auto auto;
     background: linear-gradient(to bottom, rgba(0,0,0,0), rgba(0,0,0,0.7) 60%, rgba(0,0,0,1));
     backdrop-filter: blur(4px);
-    z-index: 0;
     overflow: hidden;
 }
 
-.halo {
-    width: 40%;
-    height: 30%;
-    position: absolute;
-    bottom: -15%;
-    left: v-bind(haloPos+'%'); /* -3.5 30 56.5*/
-    background: radial-gradient(rgba(230, 182, 23, 0.8) 0%, rgba(0, 0, 0, 0) 70%);
-    z-index: 1;
+.holder {
+    display: flex;
+    justify-content: space-between;
+    margin: 0 2.5rem;
 }
 
-.footer-element {
-    margin-top: 10px;
-    margin-left: 1.66vw;
-    margin-right: 1.66vw;
-    margin-bottom: 5px;
-    width: 30vw;
-    z-index: 2;
-}
-
-.footer-element::after {
+.holder::after {
     content: '';
     position: absolute;
     width: 90vw;
@@ -86,31 +67,21 @@ const dietClick = () => {
     border-radius: 2px;
 }
 
+.footer-element {
+    margin-top: 10px;
+    margin-bottom: 5px;
+}
 .footer-element img {
     width: 60px;
     aspect-ratio: 1;
-    
 }
 
-.footer-element p {
-    font-size: 0.7rem;
-    margin: 0 0 5px 0;
-}
-
-.active img {
+.active {
     filter: invert(76%) sepia(72%) saturate(614%) hue-rotate(348deg) brightness(94%) contrast(92%);
 }
 
-.active p {
-    color: #E6B617;
-}
-
-.in-active img {
+.in-active {
     filter: invert(99%) sepia(6%) saturate(25%) hue-rotate(239deg) brightness(109%) contrast(87%)
-}
-
-.in-active p {
-    color: #eee;
 }
 
 </style>
