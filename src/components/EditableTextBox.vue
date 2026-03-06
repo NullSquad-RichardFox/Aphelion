8a420889c1b5925148aca6b55f9d0263eccbd521
NullSquad-RichardFox <richardfox.aurora@gmail.com>
2026-03-06 10:11:51 +0100

    Added editable text box class as utility for exercise and workout title in editmode

---
+++ src/components/EditableTextBox.vue
@@ -0,0 +1,29 @@
+<script setup>
+const model = defineModel();
+const props = defineProps({
+    auxiliaryText: String,
+    type: String
+})
+
+const setText = (e) => {
+    if (props.type.toLowerCase().trim() === 'number') {
+        model.value = Number(e.target.innerText.trim());
+    } else {
+        model.value = e.target.innerText.trim();
+    }
+}
+
+</script>
+
+<template>
+<p>
+    <span role="textbox" contenteditable="true" @focusout="setText">
+        {{ model }}
+    </span>
+    {{ props.auxiliaryText }}
+</p>
+</template>
+
+<style scoped>
+
+</style>
